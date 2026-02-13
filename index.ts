0ace56850b825951c1189e5910862be230787d36
prodigy-genes <bulletoze@gmail.com>
2026-02-13 11:05:16 +0000

    Hosting on vercel 4th try

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import helmet from 'helmet';
 import type {Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -19,7 +18,6 @@ export const pool = new pg.Pool({
   connectionTimeoutMillis: 5000,
 });
 
-app.use(helmet());
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
